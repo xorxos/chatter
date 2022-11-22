78816b51d69a251d599aa93873f1edfb3c69de85
Parker <parkertleavitt@gmail.com>
2022-11-22 13:49:55 -0600

    update dirname path

--- server/server.js
+++ server/server.js
@@ -91,7 +91,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
 });
 
 const start = async () => {
