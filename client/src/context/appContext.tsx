4306404d2e0b15f069a5bdeebb5b414ba3fe9665
Parker <parkertleavitt@gmail.com>
2022-11-22 19:06:59 -0600

    modify io connection request

--- client/src/context/appContext.tsx
+++ client/src/context/appContext.tsx
@@ -28,7 +28,7 @@ const AppProvider: React.FC<Props> = ({ children }) => {
         return faker.internet.color(250, 250, 250)
     }
     const connect = () => {
-        return io('http://localhost:5000', {
+        return io('ws://localhost:5000', {
             reconnectionAttempts: 5,
         })
     }
@@ -55,7 +55,7 @@ const AppProvider: React.FC<Props> = ({ children }) => {
                 messages,
                 setMessages,
                 connectChat,
-                setUserName
+                setUserName,
             }}
         >
             {children}
